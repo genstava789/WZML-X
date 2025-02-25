4e0ec55107af0622542384b835dd8069d519288f
genstava789 <ryuzakinear154@gmail.com>
2025-02-26 05:15:15 +0700

    Update button_build.py
    Signed-off-by: genstava789 <ryuzakinear154@gmail.com>

--- bot/helper/telegram_helper/button_build.py
+++ bot/helper/telegram_helper/button_build.py
@@ -1,48 +1,38 @@
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
 
 class ButtonMaker:
     def __init__(self):
         self.buttons = {
             "default": [],
             "header": [],
-            "footer": [],
+            "footer": []
         }
 
-    def ubutton(self, key, link, position=None):
-        button = InlineKeyboardButton(text=key, url=link)
-        if not position:
-            self.buttons["default"].append(button)
-        elif position == "header":
-            self.buttons["header"].append(button)
-        elif position == "footer":
-            self.buttons["footer"].append(button)
-
     def url_button(self, key, link, position=None):
-        self.buttons[position if position in self.buttons else "default"].append(
-            InlineKeyboardButton(text=key, url=link)
-        )
+        position = position if position in self.buttons else "default"
+        self.buttons[position].append(InlineKeyboardButton(text=key, url=link))
 
     def data_button(self, key, data, position=None):
-        self.buttons[position if position in self.buttons else "default"].append(
-            InlineKeyboardButton(text=key, callback_data=data)
-        )
+        position = position if position in self.buttons else "default"
+        self.buttons[position].append(InlineKeyboardButton(text=key, callback_data=data))
 
     def build_menu(self, b_cols=1, h_cols=8, f_cols=8):
         menu = [
             self.buttons["default"][i: i + b_cols] for i in range(0, len(self.buttons["default"]), b_cols)
         ]
-
+        
         if self.buttons["header"]:
             h_cnt = len(self.buttons["header"])
             if h_cnt > h_cols:
                 header_buttons = [
                     self.buttons["header"][i: i + h_cols]
-                    for i in range(0, h_cnt, h_cols)
+                    for i in range(0, len(self.buttons["header"]), h_cols)
                 ]
                 menu = header_buttons + menu
             else:
                 menu.insert(0, self.buttons["header"])
-
+        
         if self.buttons["footer"]:
             if len(self.buttons["footer"]) > f_cols:
                 [
@@ -51,10 +41,12 @@ class ButtonMaker:
                 ]
             else:
                 menu.append(self.buttons["footer"])
-
+        
         return InlineKeyboardMarkup(menu)
 
     def reset(self):
-        """Reset all button collections to an empty state."""
-        for key in self.buttons:
-            self.buttons[key].clear()
+        self.buttons = {
+            "default": [],
+            "header": [],
+            "footer": []
+        }
