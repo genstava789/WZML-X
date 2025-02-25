4e827b046238a07a9bc9162a2c4a35aa31f41cc0
genstava789 <ryuzakinear154@gmail.com>
2025-02-25 12:31:40 +0700

    Update __main__.py
    Signed-off-by: genstava789 <ryuzakinear154@gmail.com>

--- bot/__main__.py
+++ bot/__main__.py
@@ -38,6 +38,28 @@ async def stats(client, message):
     msg, btns = await get_stats(message)
     await sendMessage(message, msg, btns, photo='IMAGES')
 
+async def katbin_paste(text: str) -> str:
+    """
+	paste the text in katb.in website.
+	"""
+
+    katbin_url = "https://katb.in"
+    client = httpx.AsyncClient()
+    response = await client.get(katbin_url)
+
+    soup = BeautifulSoup(response.content, "html.parser")
+    csrf_token = soup.find('input', {"name": "_csrf_token"}).get("value")
+
+    try:
+        paste_post = await client.post(katbin_url, data={"_csrf_token": csrf_token, "paste[content]": text},
+                                       follow_redirects=False)
+        output_url = f"{katbin_url}{paste_post.headers['location']}"
+        await client.aclose()
+        return output_url
+
+    except:
+        return "something went wrong while pasting text in katb.in."
+
 @new_task
 async def start(client, message):
     buttons = ButtonMaker()
