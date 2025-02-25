722207eeb400f8e8d90d55e17a3a928bfd935859
genstava789 <ryuzakinear154@gmail.com>
2025-02-25 11:50:59 +0700

    Update mediainfo.py
    Signed-off-by: genstava789 <ryuzakinear154@gmail.com>

--- bot/modules/mediainfo.py
+++ bot/modules/mediainfo.py
@@ -14,6 +14,28 @@ from ..helper.telegram_helper.bot_commands import BotCommands
 from ..helper.telegram_helper.message_utils import send_message, edit_message
 
 
+async def katbin_paste(text: str) -> str:
+    """
+	paste the text in katb.in website.
+	"""
+
+    katbin_url = "https://katb.in"
+    client = httpx.AsyncClient()
+    response = await client.get(katbin_url)
+
+    soup = BeautifulSoup(response.content, "html.parser")
+    csrf_token = soup.find('input', {"name": "_csrf_token"}).get("value")
+
+    try:
+        paste_post = await client.post(katbin_url, data={"_csrf_token": csrf_token, "paste[content]": text},
+                                       follow_redirects=False)
+        output_url = f"{katbin_url}{paste_post.headers['location']}"
+        await client.aclose()
+        return output_url
+
+    except:
+        return "something went wrong while pasting text in katb.in."
+        
 async def gen_mediainfo(message, link=None, media=None, mmsg=None):
     temp_send = await send_message(message, "<i>Generating MediaInfo...</i>")
     try:
@@ -54,7 +76,7 @@ async def gen_mediainfo(message, link=None, media=None, mmsg=None):
         await aioremove(des_path)
     link_id = (await telegraph.create_page(title="MediaInfo X", content=tc))["path"]
     await temp_send.edit(
-        f"<b>MediaInfo:</b>\n\n➲ <b>Link :</b> https://graph.org/{link_id}",
+        f"<b>MediaInfo:</b>\n\n➲ <b>Link :</b> https://katb.in/{link_id}",
         disable_web_page_preview=False,
     )
 
